5480499735c50aa09d3a3a1d01b0e20dbde6a1ec
Hoàng Văn Biểu <nhakho@Hoangs-MBP.net.fpt>
2018-09-07 03:08:01 +0700

    update

---
+++ plugins/xeor/octocart/updates/builder_table_update_xeor_octocart_products_2.php
@@ -0,0 +1,27 @@
+<?php namespace Xeor\OctoCart\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateXeorOctocartProducts2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('xeor_octocart_products', function($table)
+        {
+            $table->string('thuonghieu', 100)->nullable();
+            $table->string('namsx', 100)->nullable();
+            $table->text('mota')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('xeor_octocart_products', function($table)
+        {
+            $table->dropColumn('thuonghieu');
+            $table->dropColumn('namsx');
+            $table->dropColumn('mota');
+        });
+    }
+}
