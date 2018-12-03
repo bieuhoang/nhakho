d05c3365f96bf119a1d5f88a87d2db4f4c0918c8
bieuhgv <bieuhv@gmail.com>
2018-12-03 20:10:16 +0700

    plg

---
+++ plugins/xeor/octocart/updates/builder_table_update_xeor_octocart_products_6.php
@@ -0,0 +1,23 @@
+<?php namespace Xeor\OctoCart\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateXeorOctocartProducts6 extends Migration
+{
+    public function up()
+    {
+        Schema::table('xeor_octocart_products', function($table)
+        {
+            $table->integer('price')->nullable()->unsigned(false)->default(0)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('xeor_octocart_products', function($table)
+        {
+            $table->decimal('price', 7, 2)->nullable(false)->unsigned(false)->default(0.00)->change();
+        });
+    }
+}
